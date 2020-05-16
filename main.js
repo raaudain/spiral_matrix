418e288d7cd90f9fb2c4281729c471b92013a56a
Ramon Audain <raudain123@gmail.com>
2020-05-16 05:58:41 +0000

    Solution

--- main.js
+++ main.js
@@ -6,7 +6,54 @@
 //  [7, 6, 5]]
 
 function matrix(n){
+  const results = [];
 
+  for (let i = 0; i < n; i++){
+    // Push empty arrays
+    results.push([]);
+  }
+  // Keeps track of what number is being pushed into results array
+  let counter = 1;
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+
+  while (startColumn <= endColumn && startRow <= endRow){
+    // Top row 
+    for (let i = startColumn; i <= endColumn; i++){
+      results[startRow][i] = counter;
+      counter++;
+      console.log("top row", results)
+    }
+    startRow++;
+
+    // Right column 
+    for (let i = startRow; i <= endRow; i++){
+      results[i][endColumn] = counter;
+      counter++
+      console.log("right column", results)
+    }
+    endColumn--;
+
+    // Bottom row
+    for (let i = endColumn; i >= startColumn; i--){
+      results[endRow][i] = counter;
+      counter++;
+      console.log("bottom row", results)
+    }
+    endRow--;
+
+    // start column
+    for (let i = endRow; i >= startRow; i--){
+      results[i][startColumn] = counter;
+      counter++
+      console.log("left column", results)
+    }
+    startColumn++;
+  }
+
+  return results;
 }
 
-matrix(3)
+matrix(3);
