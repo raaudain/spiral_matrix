6ed11074127ca8240f3689ca18ecdd8c04b9eb9c
Ramon Audain <raudain123@gmail.com>
2020-05-16 05:17:31 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// Write a function that accepts an integer N and returns a NxN sprial matrix.
+// Example
+// matrix(3)
+//  [[1, 2, 3],
+//  [8, 9, 4],
+//  [7, 6, 5]]
+
+function matrix(n){
+
+}
+
+matrix(3)
